30efa27fcb56cd41562b2f6dfa8583c0a0d49e27
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-06-17 16:38:26 +0100

    added returns to Falkirk High

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -17,6 +17,10 @@ stops.delete_at(2)
 #7. Print the number of stops there are in the array?
 p stops.length()
 #8. Show as many ways as you can to return "Falkirk High" from the array?
+p stops[2]
+p stops[-5]
+p stops.slice!(2)
+stops.insert(2, "Falkirk High")
 #9. Reverse the positions of the stops in the array
 #10 Print out all the stops using a for loop
 
