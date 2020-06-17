dc2963baee78c1b03adc653f74e583d162a84025
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-06-17 18:15:08 +0100

    added list of each country

--- exercise_c_uk.rb
+++ exercise_c_uk.rb
@@ -20,7 +20,11 @@ united_kingdom = [
 p united_kingdom[1][:capital] = "Cardiff"
 
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
-p united_kingdom << {name: "Northern Ireland", population: "1811000", capital: "Belfast"}
+united_kingdom << {name: "Northern Ireland", population: "1811000", capital: "Belfast"}
 
 # 3. Use a loop to print the names of all the countries in the UK.
+for country in united_kingdom
+  p country[:name]
+end
+
 # 4. Use a loop to find the total population of the UK.
